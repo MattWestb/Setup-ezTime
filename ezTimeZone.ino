d213ede2b98aa84f6d310a5bd6fd84ead3ca5ed5
MattWestb <49618193+MattWestb@users.noreply.github.com>
2020-02-28 12:38:49 +0100

    Update ezTimeZone.ino

--- ezTimeZone.ino
+++ ezTimeZone.ino
@@ -5,8 +5,8 @@ Timezone Austria; // A name of Your time zone and inisiating it
 
 void setup() {
     Serial.begin(115200);
-    WiFi.begin("Your SSID", "Your Password");
-    Austria.setPosix("CET-1CEST,M3.5.0,M10.5.0/3"); //Posix for Your time zone
+    WiFi.begin("Your SSID", "Your Password"); // Your Credentials
+    Austria.setPosix("CET-1CEST,M3.5.0,M10.5.0/3"); // Posix for Your time zone
     Austria.setDefault(); // Set Your time zone as default
     setServer("ntp.se"); // Set Your NTP serverpool
     // Uncomment the line below to see what it does behind the scenes
